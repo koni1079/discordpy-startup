f0fa001fdaf4e5d96c383d1d2052dff61599a8ca
koni1079 <62229778+koni1079@users.noreply.github.com>
2020-05-07 16:52:26 +0900

    Update kaiseki.py

--- kaiseki.py
+++ kaiseki.py
@@ -1,57 +1,28 @@
-import MeCab
+from janome.tokenizer import Tokenizer
 
 def analysis(message):
+    t=Tokenizer()
     morpheme=[]
     taboo={"タイトル":0,"作者":1,"日付":2} #発売日は辞書登録しなければならないため一応日付
     tabooset = 0
-    m=MeCab.Tagger("-Ochasen")
-    m.parse('')
-    #message = message.encode('utf-8')
-    node = m.parseToNode(message)
-    
-    while node:
-        word=node.surface
-        #print(word)
-        #print("----")
-        wclass=node.feature.split(",")
-        if wclass[0] != u'BOS/EOS':
-            if wclass[0] == "名詞":
-                tabooflag = 0
-                if tabooset != 1:
-                    if word in taboo.keys():
-                        morpheme.insert(0,taboo[word])
-                        tabooflag = 1
-                        tabooset = 1
 
-                if tabooflag != 1:    
-                    #print(noun)
-                    morpheme.append(word)
-                    #print(word)
-                    #print(word,",",wclass[0])
-        #print(word,end=",")
-        #print(wclass)   #ここで形態素解析表示できる
-        node=node.next
-    #print(wclass[0])
-    #print(morpheme)
+    for token in t.tokenize(message):
+        if token.part_of_speech.split(',')[0] == "名詞":
+            tabooflag = 0
+            if tabooset != 1:
+                if token.surface in taboo.keys():
+                    morpheme.insert(0,taboo[token.surface])
+                    tabooflag = 1
+                    tabooset = 1
+            if tabooflag != 1:
+                morpheme.append(token.surface)
     if tabooset != 1:
         morpheme.insert(0,0)
-    return morpheme #リスト型[“名詞1”,”名詞2”,…]
-    
-    #return m.parse(message)
+    return morpheme
 
 def wakati(title):
+    t=Tokenizer()
     morpheme=[]
-    m=MeCab.Tagger("-Ochasen")
-    m.parse('')
-    node = m.parseToNode(title)
-    while node:
-        word=node.surface
-        wclass=node.feature.split(",")
-        if wclass[0] != u'BOS/EOS':
-            morpheme.append(word)
-            #print(word)
-            #print(word,",",wclass[0])
-        node=node.next
-    #print(wclass[0])
-    #print(morpheme)
-    return morpheme #リスト型[“名詞1”,”名詞2”,…]
+    for token in t.tokenize(title):
+        morpheme.append(token.surface)
+    return morpheme
