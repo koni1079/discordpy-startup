1820e8686eeaa200fb033ad753e6468f2321325f
koni1079 <62229778+koni1079@users.noreply.github.com>
2020-05-07 16:51:07 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -3,7 +3,7 @@ import discord
 #import web_sample
 #import kaiseki
 #import yahoo_search
-from janome.tokenizer import Tokenizer
+#from janome.tokenizer import Tokenizer
 from discord.ext import commands
 import os
 import traceback
