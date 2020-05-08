39134017533eb953d0bb64b3534b776233bff7bd
koni1079 <62229778+koni1079@users.noreply.github.com>
2020-05-09 00:23:22 +0900

    Update search_book.py

--- search_book.py
+++ search_book.py
@@ -1,7 +1,9 @@
 import sys
 import kaiseki
 book_list = []
+wakati_list = []
 path_r = "./book_data.txt"
+path_r2 = "./wakati_data.txt"
 
 def setup_list():
     f = open(path_r,"r",encoding = 'utf-8')
@@ -11,11 +13,24 @@ def setup_list():
         line = line.split(",")
         book_list.append(line)
     f.close()
-    
+
+    f = open(path_r2,"r",encoding = 'utf-8')
+    one_book = []
+    for line in f:
+        line = line.strip()
+        line = line.replace("\n","")
+        line = line.split(",")
+        one_book.append(line)
+        if len(one_book) == 3:
+            wakati_list.append(one_book)
+            one_book = [] 
+    f.close()
+    #print(wakati_list)
+
+
 def search(book):
     if book[0] == 0:
         book.pop(0)
-        return book_list[-1]
         return search_title(book)
     elif book[0] == 1:
         book.pop(0)
@@ -29,34 +44,34 @@ def search_title(book_name):
     result=0
     serched_list=[]
     if book_name != []:
-        for i in book_list:
-             if set(book_name) <=  set(kaiseki.wakati(i[0])): #i[0]タイトル検索 i[1]日付検索 i[2]著者検索
-                serched_list.append(i)
+        for i,book in enumerate(wakati_list):
+            #print(title[0])
+            if set(book_name) <=  set(book[0]): #i[0]タイトル検索 i[1]日付検索 i[2]著者検索
+                serched_list.append(book_list[i])
                 result=1
     if result != 1:
-       return False
+        #print(kaiseki.wakati(book_list[-1][0]))
+        return False
     return serched_list
 
 def search_date(book_date):
     result=0
     serched_list=[]
-    for i in book_list:
-        if set(book_date) <=  set(kaiseki.wakati(i[1])): #i[0]タイトル検索 i[1]日付検索 i[2]著者検索
-            serched_list.append(i)
+    for i,book in enumerate(wakati_list):
+        if set(book_date) <=  set(book[1]): #i[0]タイトル検索 i[1]日付検索 i[2]著者検索
+            serched_list.append(book_list[i])
             result=1
     if result != 1:
-        return False
-        
+        return False       
     return serched_list
 
 def search_author(book_author):
     result=0
     serched_list=[]
-    for i in book_list:
-        if set(book_author) <=  set(kaiseki.wakati(i[2])): #i[0]タイトル検索 i[1]日付検索 i[2]著者検索
-            serched_list.append(i)
+    for i,book in enumerate(wakati_list):
+        if set(book_author) <=  set(book[2]): #i[0]タイトル検索 i[1]日付検索 i[2]著者検索
+            serched_list.append(book_list[i])
             result=1    
     if result != 1:
         return False
-
     return serched_list
